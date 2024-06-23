5df3c8d94b742eee061ea47244cf9857512b3124
vopula <alkasidikprawira12@gmail.com>
2024-06-23 10:43:55 +0700

    feat: add contact, footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,42 +1,31 @@
 import React from "react";
-import Logo from "../Assets/Logo.svg";
-import { BsTwitter } from "react-icons/bs";
-import { SiLinkedin } from "react-icons/si";
-import { BsYoutube } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
+
+import marketplaceicon from "../Assets/icons8-shopee-48kecil.png";
+import whatsappicon from "../Assets/icons8-whatsapp-48kecil.png";
 
 const Footer = () => {
   return (
     <div className="footer-wrapper">
       <div className="footer-section-one">
         <div className="footer-logo-container">
-          <img src={Logo} alt="" />
+          <h4>Sinar Intan Toys</h4>
         </div>
         <div className="footer-icons">
-          <BsTwitter />
-          <SiLinkedin />
-          <BsYoutube />
-          <FaFacebookF />
+          <img src={marketplaceicon} alt=""/>
+          <img src={whatsappicon} alt=""/>
         </div>
       </div>
       <div className="footer-section-two">
         <div className="footer-section-columns">
-          <span>Qualtiy</span>
-          <span>Help</span>
-          <span>Share</span>
-          <span>Carrers</span>
-          <span>Testimonials</span>
+          <span>Home</span>
+          <span>About</span>
           <span>Work</span>
+          <span>Testimonials</span>
+          <span>Contact</span>
         </div>
         <div className="footer-section-columns">
-          <span>244-5333-7783</span>
-          <span>hello@food.com</span>
-          <span>press@food.com</span>
-          <span>contact@food.com</span>
-        </div>
-        <div className="footer-section-columns">
-          <span>Terms & Conditions</span>
-          <span>Privacy Policy</span>
+          <span>0823-2301-1032</span>
+          <span>TommyGunawan@gmail.com</span>
         </div>
       </div>
     </div>
